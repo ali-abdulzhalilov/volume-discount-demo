e0efd21f8b1e415dac7a056ebfbae21ec8448a09
Ali Abdulzhalilov <mark.an.impostor@gmail.com>
2021-05-10 15:56:02 +0300

    slider & other refactoring

--- src/components/HorizontalSlider.js
+++ src/components/HorizontalSlider.js
@@ -1,49 +1,52 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {Drag} from "@visx/drag";
 import {Line} from "@visx/shape";
 
-function HorizontalSlider({top, left, width, height, value, isDragging, onDrag}) {
+function HorizontalSlider({top, left, width, height, value, onDrag}) {
+    const [isDragging, setIsDragging] = useState(false);
+    const onDragWrapper = (c) => {setIsDragging(c.isDragging); onDrag(c);}
+
     return (
         <>
             <g transform={`translate(${left},${top})`}>
-            <Drag
-                x={value}
-                y={0}
-                width={width}
-                height={height}
-                onDragStart={onDrag}
-                onDragMove={onDrag}
-                onDragEnd={onDrag}
-            >
-                {({ dragStart, dragEnd, dragMove, isDragging, x, dx }) => (
-                    <rect
-                        key={`dot-single`}
-                        x={x-5-left}
-                        y={0}
-                        width={10}
-                        height={height}
-                        fill={"transparent"}
-                        transform={`translate(${dx}, 0)`}
-                        fillOpacity={0.9}
-                        stroke={isDragging ? "transparent" : "#eee"}
-                        strokeWidth={1}
-                        onMouseMove={dragMove}
-                        onMouseUp={dragEnd}
-                        onMouseDown={dragStart}
-                        onTouchStart={dragStart}
-                        onTouchMove={dragMove}
-                        onTouchEnd={dragEnd}
-                    />
-                )}
-            </Drag>
+                <Drag
+                    x={value}
+                    y={0}
+                    width={width}
+                    height={height}
+                    onDragStart={onDragWrapper}
+                    onDragMove={onDragWrapper}
+                    onDragEnd={onDragWrapper}
+                >
+                    {({dragStart, dragEnd, dragMove, isDragging, x, dx}) => (
+                        <rect
+                            key={`dot-single`}
+                            x={x - 5 - left}
+                            y={0}
+                            width={10}
+                            height={height}
+                            fill={"transparent"}
+                            transform={`translate(${dx}, 0)`}
+                            fillOpacity={0.9}
+                            stroke={isDragging ? "transparent" : "#eee"}
+                            strokeWidth={1}
+                            onMouseMove={dragMove}
+                            onMouseUp={dragEnd}
+                            onMouseDown={dragStart}
+                            onTouchStart={dragStart}
+                            onTouchMove={dragMove}
+                            onTouchEnd={dragEnd}
+                        />
+                    )}
+                </Drag>
 
-            <Line
-                from={{x: value-left, y: 0}}
-                to={{x: value-left, y: height}}
-                stroke="#f66"
-                strokeWidth={isDragging ? 2 : 1}
-                pointerEvents="none"
-            />
+                <Line
+                    from={{x: value - left, y: 0}}
+                    to={{x: value - left, y: height}}
+                    stroke="#f66"
+                    strokeWidth={isDragging ? 2 : 1}
+                    pointerEvents="none"
+                />
             </g>
         </>
     );
--- src/components/SmoothVolumeChart.js
+++ src/components/SmoothVolumeChart.js
@@ -4,32 +4,34 @@ import {curveLinear} from "@visx/curve";
 import {scaleLinear} from '@visx/scale';
 import {HorizontalSlider} from "../components";
 
+const poly = (a, b, c, n, x) => a * Math.pow(x + b, n) + c;
+const fun = (x) => poly(0.01, 0, 5, 1.8, x);
+
 function SmoothVolumeChart({top, left, width, height, pointCount, maxPossibleAmount}) {
 
-    const poly = (a, b, c, n, x) => a * Math.pow(x + b, n) + c;
-    const fun = useCallback((x) => poly(0.01, 0, 0, 1.9, x), []);
+    const [maxAmount, setMaxAmount] = useState(70);
+    const [maxDiscount, setMaxDiscount] = useState(fun(maxAmount));
+    useEffect(() => setMaxDiscount(fun(maxAmount)), [maxAmount]);
 
-    const [maxX, setMaxX] = useState(70);
-    const [maxY, setMaxY] = useState(fun(maxX));
-    useEffect(() => {setMaxY(fun(maxX))}, [fun, maxX]);
-    const funWithLimit = (x) => Math.min(fun(x), maxY);
+    const [minAmount, setMinAmount] = useState(10);
 
-    const dn = maxPossibleAmount/pointCount;
-    const data = Array.from({length: pointCount}, (x, i) => ({x: i*dn, y: funWithLimit(i*dn)}));
+    const funLimited = (x) => x < minAmount ? 0 : Math.max(Math.min(fun(x), maxDiscount), 0);
+
+    const dn = maxPossibleAmount / pointCount;
+    const data = Array.from({length: pointCount}, (x, i) => ({x: i * dn, y: funLimited(i * dn)}));
 
     const xScale = scaleLinear({range: [0, width], domain: [0, maxPossibleAmount]});
     const yScale = scaleLinear({range: [height, 0], domain: [0, 100]});
 
-    const getScaledX = (d) => xScale(d.x) ?? 0;
-    const getScaledY = (d) => yScale(d.y) ?? 0;
+    const getValue = useCallback((value) => value / width * maxPossibleAmount, [maxPossibleAmount, width]);
 
     // -----
 
-    const [xx, setXX] = useState(xScale(maxX)+left);
-    const [isDragging, setIsDragging] = useState(false);
-    const getValue = (value) => value/width*maxPossibleAmount;
+    const [maxX, setMaxX] = useState(xScale(maxAmount) + left);
+    useEffect(() => setMaxAmount(getValue(maxX - left)), [getValue, maxX, left]);
 
-    const onDrag = c => {setXX(c.x+c.dx); setIsDragging(c.isDragging); getValue(c.x+c.dx-left); setMaxX(getValue(c.x+c.dx-left))};
+    const [minX, setMinX] = useState(xScale(minAmount) + left);
+    useEffect(() => setMinAmount(getValue(minX - left)), [getValue, minX, left]);
 
     // -----
 
@@ -39,8 +41,8 @@ function SmoothVolumeChart({top, left, width, height, pointCount, maxPossibleAmo
                 <LinePath
                     curve={curveLinear}
                     data={data}
-                    x={getScaledX}
-                    y={getScaledY}
+                    x={(d) => xScale(d.x) ?? 0}
+                    y={(d) => yScale(d.y) ?? 0}
                     strokeWidth={2}
                     stroke="#000"
                 />
@@ -51,9 +53,17 @@ function SmoothVolumeChart({top, left, width, height, pointCount, maxPossibleAmo
                 left={left}
                 width={width}
                 height={height}
-                value={xx}
-                isDragging={isDragging}
-                onDrag={onDrag}
+                value={maxX}
+                onDrag={(c) => setMaxX(c.x + c.dx)}
+            />
+
+            <HorizontalSlider
+                top={top}
+                left={left}
+                width={width}
+                height={height}
+                value={minX}
+                onDrag={(c) => setMinX(c.x + c.dx)}
             />
         </>
     );
